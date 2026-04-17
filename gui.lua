794a1e361c7ff0c45a4e37bf09381a795d9d9c61
bunsass <anna1410thu@gmail.com>
2026-04-17 20:22:00 +0700

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,3 +1,38 @@
-game:GetService("GuiService").ErrorMessageChanged:Connect(function()
-wait (2) game:GetService("TeleportService"):Teleport(12886143095)
-    end)
+-- Auto-Reconnect to Same Server on Error GUI
+-- Rejoins the current server when ErrorMessageChanged is triggered
+
+local TeleportService = game:GetService("TeleportService")
+local GuiService = game:GetService("GuiService")
+local Players = game:GetService("Players")
+
+local player = Players.LocalPlayer
+local RECONNECT_DELAY = 2 -- seconds before teleporting
+
+print("[Auto-Reconnect] Script started!")
+print("[Auto-Reconnect] Current Place ID:", game.PlaceId)
+print("[Auto-Reconnect] Current Job ID:", game.JobId)
+
+-- Function to get current server job ID
+local function getCurrentJobId()
+    return game.JobId
+end
+
+-- Listen for error GUI
+GuiService.ErrorMessageChanged:Connect(function()
+    local errorMessage = GuiService:GetErrorMessage()
+    
+    if errorMessage ~= "" then
+        print("[Auto-Reconnect] Error detected:", errorMessage)
+        print("[Auto-Reconnect] Attempting to rejoin in", RECONNECT_DELAY, "seconds...")
+        
+        wait(RECONNECT_DELAY)
+        
+        -- Rejoin the same server using current JobId
+        local currentJobId = getCurrentJobId()
+        print("[Auto-Reconnect] Rejoining server with Job ID:", currentJobId)
+        
+        TeleportService:TeleportToPlaceInstance(game.PlaceId, currentJobId, player)
+    end
+end)
+
+print("[Auto-Reconnect] Monitoring for errors...")
