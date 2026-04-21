176ba802c4e0aa9334ada2f0e6ec94222163c834
bunsass <anna1410thu@gmail.com>
2026-04-22 00:05:37 +0700

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,3 +1,31 @@
-game:GetService("GuiService").ErrorMessageChanged:Connect(function()
-wait (2) game:GetService("TeleportService"):Teleport(77747658251236)
-    end)
+local UIS = game:GetService("UserInputService")
+
+local running = true
+
+-- Toggle with P
+UIS.InputBegan:Connect(function(input)
+    if input.KeyCode == Enum.KeyCode.P then
+        running = not running
+        warn("Auto skill: " .. (running and "ON" or "OFF"))
+    end
+end)
+
+task.spawn(function()
+    while task.wait(0.1) do
+        if running then
+            -- Press X
+            keypress(0x58)
+            task.wait(0.05)
+            keyrelease(0x58)
+            
+            task.wait(0.05)
+            
+            -- Press C
+            keypress(0x43)
+            task.wait(0.05)
+            keyrelease(0x43)
+        end
+    end
+end)
+
+warn("Auto skill loaded! Press P to toggle ON/OFF.")
